3372d62cf4eb9f9fbd516155491af2245aaf6591
ssverchkov <ssverchkov@tango.me>
2025-09-13 22:49:48 +0200

    fix logger

--- composeApp/src/commonMain/kotlin/com/slimdroid/movies/network/NetworkClient.kt
+++ composeApp/src/commonMain/kotlin/com/slimdroid/movies/network/NetworkClient.kt
@@ -2,10 +2,7 @@ package com.slimdroid.movies.network
 
 import com.slimdroid.movies.BuildConfig.TMDB_TOKEN
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
@@ -18,8 +15,6 @@ import io.ktor.http.ContentType
 import io.ktor.http.URLProtocol
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
 object NetworkClient {
@@ -64,30 +59,8 @@ object NetworkClient {
                 requestTimeoutMillis = 5_000
                 connectTimeoutMillis = 2_000
             }
-            HttpResponseValidator {
-                handleResponseExceptionWithRequest { cause, _ ->
-                    val clientException = cause as? ClientRequestException
-                        ?: return@handleResponseExceptionWithRequest
-                    val exceptionResponse = clientException.response
-                    val error: Error = exceptionResponse.body()
-                    co.touchlab.kermit.Logger.e(clientException, TAG) {
-                        "HTTP Error ${error.statusCode} ${error.statusMessage}"
-                    }
-                    throw HttpClientException(error)
-                }
-            }
         }
     }
 }
 
 expect fun getHttpEngine(): HttpClientEngine
-
-@Serializable
-data class Error(
-    @SerialName("status_code") val statusCode: Int,
-    @SerialName("status_message") val statusMessage: String,
-    @SerialName("success") val success: Boolean
-)
-
-data class HttpClientException(val error: Error) :
-    IllegalStateException("HTTP Error ${error.statusCode}")
