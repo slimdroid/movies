19cffc52f07955bcba594f74ba9a62dd8905ab30
ssverchkov <ssverchkov@tango.me>
2025-09-14 12:48:14 +0200

    improve resolve url on ios

--- composeApp/src/iosMain/kotlin/com/slimdroid/movies/common/urlResolver.ios.kt
+++ composeApp/src/iosMain/kotlin/com/slimdroid/movies/common/urlResolver.ios.kt
@@ -1,16 +1,46 @@
 package com.slimdroid.movies.common
 
+import co.touchlab.kermit.Logger
 import platform.Foundation.NSURL
 import platform.UIKit.UIApplication
 
 actual fun openMovieTrailer(url: String) {
-    // FIXME doesn't work on simulator
-    val nsUrl = NSURL(string = url)
-    nsUrl?.let {
-        if (UIApplication.sharedApplication.canOpenURL(it)) {
-            UIApplication.sharedApplication.openURL(it)
-        } else {
-            println("Cannot open URL: $url")
+    val nsUrl = NSURL(string = url) ?: return
+
+    if (url.contains("youtube.com/watch?v=")) {
+        tryOpenYouTubeApp(url)
+    } else {
+        openInBrowser(nsUrl)
+    }
+}
+
+private fun openInBrowser(url: NSURL) {
+    UIApplication.sharedApplication.openURL(
+        url = url,
+        options = emptyMap<Any?, Any>(),
+        completionHandler = { success: Boolean ->
+            if (success.not()) {
+                Logger.w { "❌ Cannot open URL: $url" }
+            }
         }
+    )
+}
+
+private fun tryOpenYouTubeApp(url: String) {
+    val videoId = url
+        .substringAfter("v=", missingDelimiterValue = "")
+        .substringBefore("&")
+
+    NSURL(string = "youtube://$videoId")?.let { youtubeUrl ->
+        UIApplication.sharedApplication.openURL(
+            youtubeUrl,
+            options = emptyMap<Any?, Any>(),
+            completionHandler = { success ->
+                if (success.not()) {
+                    // If YouTube doesn't exist → open browser
+                    openInBrowser(NSURL(string = "https://www.youtube.com/watch?v=$videoId")!!)
+                }
+            }
+        )
     }
 }
